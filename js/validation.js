84b9fa609e991c7811623959709eaa496e171e17
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2022-08-17 13:29:43 +0200

    animal select validation
    animal select validation

--- js/validation.js
+++ js/validation.js
@@ -2,10 +2,12 @@
 const cowInput = document.getElementById("cow");
 const pigInput = document.getElementById("pig");
 const mooseInput = document.getElementById("moose");
+const animalSelect = document.getElementById("animalSelect");
 
 //validate buttons
 const validatePig = document.getElementById("evaluatePig");
 const validateAll = document.getElementById("evaluateForm");
+let animalDescription = document.getElementById("animalDescription");
 
 //eventListeners
 cowInput.addEventListener("keyup", evaluateCow);
@@ -34,6 +36,8 @@ function evaluatePig() {
 }
 
 
+
+
 function evaluateAll() {
 	if (cowInput.value == "ko") {
 		console.log("weeeeee ko");
@@ -56,7 +60,26 @@ function evaluateAll() {
 		console.log("ikke moose!");
 		mooseInput.style.backgroundColor = "red";
 	}
+	switch (animalSelect.value) {
+		case "Fugl":
+			animalDescription.innerHTML = "You have chosen " + animalSelect.value + " a warm-blooded vertebrate of the class Aves, having a body covered with feathers"
+			break;
+		case "Fisk":
+			animalDescription.innerHTML = "You have chosen " + animalSelect.value + " an aquatic, craniate, gill-bearing animal that lacks limbs with digits."
+			break;
+		case "Reptil":
+			animalDescription.innerHTML = "You have chosen " + animalSelect.value + " an animal in the class Reptilia, a paraphyletic grouping comprising all sauropsid amniotes except Aves (birds)."
+			break;
+		case "Insekt":
+			animalDescription.innerHTML = "You have chosen " + animalSelect.value + " a pancrustacean hexapod invertebrate of the class Insecta. They are the largest group within the arthropod phylum."
+			break;
+		case "Pattedyr":
+			animalDescription.innerHTML = "You have chosen " + animalSelect.value + " a vertebrate animal constituting the class Mammalia characterized by the presence of mammary glands."
+			break;
+
+
+	}
 
 }
 
-//---------------------------------functions----------<
+//---------------------------------functions------------------------<
